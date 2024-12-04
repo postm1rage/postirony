fb5391ed9b13115b982de7023d09b93471434ebe
postm1rage <pro100lights2@gmail.com>
2024-12-04 14:33:41 +0700

    fix Anim + Sound

--- Assets/Animations/Player/weapon/Weapon.cs
+++ Assets/Animations/Player/weapon/Weapon.cs
@@ -12,7 +12,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] private AudioClip shootSound; 
     [SerializeField] private AudioClip reloadSound; 
     [SerializeField] private Animator animator; 
-    [SerializeField] private SpriteRenderer weaponSprite; // Ссылка на SpriteRenderer оружия
+    [SerializeField] private SpriteRenderer weaponSprite; 
+    [SerializeField] private AudioSource audioSource; // Ссылка на AudioSource
 
     private int currentAmmo; 
     private bool isReloading = false; 
@@ -31,9 +32,15 @@ public class Weapon : MonoBehaviour
         { 
             Shoot(); 
         }
-        else if (!Input.GetButton("Fire1") && animator.GetBool("IsShooting")) // Добавлено условие
+        else if (!Input.GetButton("Fire1") && animator.GetBool("IsShooting")) 
         {
-            animator.SetBool("IsShooting", false); // Сбрасываем состояние стрельбы
+            animator.SetBool("IsShooting", false); 
+        }
+
+        // Stop shooting animation if no ammo
+        if (currentAmmo <= 0 && animator.GetBool("IsShooting"))
+        {
+            animator.SetBool("IsShooting", false);
         }
 
         if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo && !isReloading) 
@@ -42,65 +49,60 @@ public class Weapon : MonoBehaviour
         } 
     }
 
-
     void Aim()  
     {  
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);  
-        mousePosition.z = 0; // Обнуление Z координаты для 2D пространства
+        mousePosition.z = 0; 
 
         Vector3 direction = (mousePosition - transform.position).normalized;  
-
-        // Вычисляем угол для поворота
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;  
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle)); 
 
-        // Устанавливаем направление стрельбы в firePoint
         firePoint.right = direction;
 
-        // Поворачиваем и инвертируем спрайт оружия
         if (mousePosition.x < transform.position.x) 
         {
-            weaponSprite.flipX = true; // Инвертируем спрайт по X
-            // Поворачиваем на 180 градусов
+            weaponSprite.flipX = true; 
             weaponSprite.transform.rotation = Quaternion.Euler(0, 0, angle + 180); 
         } 
         else 
         {
-            weaponSprite.flipX = false; // Возвращаем в нормальное состояние
-            // Убираем поворот
+            weaponSprite.flipX = false; 
             weaponSprite.transform.rotation = Quaternion.Euler(0, 0, angle); 
         }
     }
 
     void Shoot()    
-{    
-    if (currentAmmo > 0)    
     {    
-        currentAmmo--;    
-        AudioSource.PlayClipAtPoint(shootSound, transform.position);    
-        animator.SetTrigger("IsShooting"); // Установите триггер на стреление  
-        animator.SetBool("IsShooting", true); // Устанавливаем состояние стрельбы  
+        if (currentAmmo > 0)    
+        {    
+            currentAmmo--;    
+            audioSource.PlayOneShot(shootSound); // Заменяем на PlayOneShot
+            animator.SetTrigger("IsShooting");  
+            animator.SetBool("IsShooting", true);  
   
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);    
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();    
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);    
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();    
 
-        // Получаем позицию курсора
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0; // Обнуляем Z координату для 2D пространства
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0; 
 
-        // Вычисляем направление к курсору
-        Vector2 directionToMouse = (mousePosition - firePoint.position).normalized;
+            Vector2 directionToMouse = (mousePosition - firePoint.position).normalized;
 
-        // Устанавливаем скорость пули
-        rb.linearVelocity = directionToMouse * bulletSpeed;
+            rb.linearVelocity = directionToMouse * bulletSpeed;
 
-        nextFireTime = Time.time + fireRate;   
-    }    
-}
+            nextFireTime = Time.time + fireRate;   
+        }
+        else 
+        {
+            animator.SetBool("IsShooting", false); // Stop shooting animation if no ammo
+        }    
+    }
+
     private IEnumerator Reload() 
     { 
         isReloading = true; 
-        AudioSource.PlayClipAtPoint(reloadSound, transform.position); 
+        audioSource.PlayOneShot(reloadSound); // Заменяем на PlayOneShot
         yield return new WaitForSeconds(reloadTime); 
         currentAmmo = maxAmmo; 
         isReloading = false; 
