7c0808d1956813af05c1a84314968a6a826ad120
postm1rage <pro100lights2@gmail.com>
2024-12-04 16:28:36 +0700

    WIP

---
+++ Assets/Animations/Player/weapon/Bullet.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 10;
+    private BossBehavior boss; // Declare BossBehavior variable
+
+    // Use Awake to find the Boss game object
+    void Awake()
+    {
+        // Find the Boss object and get the BossBehavior component
+        GameObject bossObject = GameObject.Find("Boss");
+        if (bossObject != null)
+        {
+            boss = bossObject.GetComponent<BossBehavior>();
+        }
+        else
+        {
+            Debug.LogError("Boss not found in the scene!");
+        }
+    }
+
+    void Start()
+    {
+        // Удалить пулю через 2 секунды
+        Destroy(gameObject, 2f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check for collision with the enemy
+        if (collision.gameObject.CompareTag("Boss"))
+        {
+            // Используем BossBehavior
+            BossBehavior bossBehavior = collision.gameObject.GetComponent<BossBehavior>();
+            if (bossBehavior != null)
+            {
+                // Вызываем метод TakeDamage у BossBehavior
+                bossBehavior.TakeDamage(damage);
+            }
+            Destroy(gameObject); // Destroy the bullet
+        }
+    }
+}
